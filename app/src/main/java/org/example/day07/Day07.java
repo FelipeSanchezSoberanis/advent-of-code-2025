fa64a4e22b6c4732f72371e204cee7035a5cc59c
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2025-12-08 10:52:11 -0600

    feat: day 07 part 01

---
+++ app/src/main/java/org/example/day07/Day07.java
@@ -0,0 +1,57 @@
+package org.example.day07;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Day07 {
+  private final String inputsDir;
+
+  public Day07() {
+    this.inputsDir =
+        Paths.get(System.getProperty("user.dir"), "src", "main", "resources", "inputs", "day07")
+            .toString();
+  }
+
+  public List<String> parseInput(String filename) throws IOException {
+    return Files.lines(Paths.get(inputsDir, filename)).collect(Collectors.toList());
+  }
+
+  public Integer solvePart01(List<String> rows) {
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    Set<Integer> beams = new HashSet<>();
+    beams.add(rows.getFirst().indexOf("S"));
+    return rows.stream()
+        .skip(1)
+        .reduce(
+            0,
+            (acc, row) -> {
+              List<Integer> splitters =
+                  IntStream.range(0, row.length())
+                      .boxed()
+                      .filter(i -> row.charAt(i) == '^')
+                      .collect(Collectors.toList());
+              splitters.forEach(
+                  splitter -> {
+                    if (splitter != row.length() - 1) beams.add(splitter + 1);
+                    if (splitter != 0) beams.add(splitter - 1);
+                  });
+              int collisions =
+                  splitters.stream().filter(beams::contains).collect(Collectors.toList()).size();
+              beams.removeAll(splitters);
+              return acc + collisions;
+            },
+            Integer::sum);
+  }
+
+  public Integer solvePart02(List<String> rows) {
+    return null;
+  }
+}
---
+++ app/src/test/java/org/example/day07/Day07Test.java
@@ -0,0 +1,39 @@
+package org.example.day07;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class Day07Test {
+  private final Day07 day07;
+  private final List<String> exampleRows;
+  private final List<String> rows;
+
+  public Day07Test() throws IOException {
+    this.day07 = new Day07();
+    this.exampleRows = day07.parseInput("input.example.txt");
+    this.rows = day07.parseInput("input.txt");
+  }
+
+  @Test
+  public void testExample01() {
+    assertEquals(21, day07.solvePart01(exampleRows));
+  }
+
+  @Test
+  public void testPart01() {
+    assertEquals(0, day07.solvePart01(rows));
+  }
+
+  // @Test
+  // public void testExample02() {
+  //   assertEquals(3263827L, day07.solvePart02(exampleRows));
+  // }
+
+  // @Test
+  // public void testPart02() {
+  //   assertEquals(9029931401920L, day07.solvePart02(rows));
+  // }
+}
