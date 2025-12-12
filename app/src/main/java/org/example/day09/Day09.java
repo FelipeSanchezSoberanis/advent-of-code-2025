1745550d5ca665b26c834458ad6fbc0002b97bc5
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2025-12-12 11:16:09 -0600

    feat: day 09 part 02 example

--- app/src/main/java/org/example/day09/Day09.java
+++ app/src/main/java/org/example/day09/Day09.java
@@ -7,7 +7,6 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import lombok.Builder;
 import lombok.Getter;
 import tools.jackson.databind.ObjectMapper;
 
@@ -25,10 +24,7 @@ public class Day09 {
         .map(
             line -> {
               String[] parts = line.split(",");
-              return Coordinate.builder()
-                  .x(Long.parseLong(parts[0]))
-                  .y(Long.parseLong(parts[1]))
-                  .build();
+              return new Coordinate(Long.parseLong(parts[0]), Long.parseLong(parts[1]));
             })
         .collect(Collectors.toList());
   }
@@ -42,10 +38,7 @@ public class Day09 {
                   .boxed()
                   .map(
                       j -> {
-                        Coordinate redTileOne = redTiles.get(i);
-                        Coordinate redTileTwo = redTiles.get(j);
-                        return (Math.abs(redTileOne.getY() - redTileTwo.getY()) + 1)
-                            * (Math.abs(redTileOne.getX() - redTileTwo.getX()) + 1);
+                        return new Rectangle(redTiles.get(i), redTiles.get(j)).getArea();
                       });
             })
         .sorted(Comparator.reverseOrder())
@@ -54,18 +47,185 @@ public class Day09 {
   }
 
   public Long solvePart02(List<Coordinate> redTiles) {
-    return null;
+    List<LineSegment> lineSegments =
+        IntStream.range(0, redTiles.size() - 1)
+            .boxed()
+            .map(
+                i -> {
+                  Coordinate redTileOne = redTiles.get(i);
+                  Coordinate redTileTwo = redTiles.get(i + 1);
+                  return new LineSegment(redTileOne, redTileTwo);
+                })
+            .collect(Collectors.toList());
+    lineSegments.add(new LineSegment(redTiles.getLast(), redTiles.getFirst()));
+
+    return IntStream.range(0, redTiles.size() - 1)
+        .boxed()
+        .flatMap(
+            i ->
+                IntStream.range(i + 1, redTiles.size())
+                    .boxed()
+                    .map(j -> new Rectangle(redTiles.get(i), redTiles.get(j))))
+        .sorted(Comparator.comparingLong(Rectangle::getArea).reversed())
+        .filter(
+            rectangle ->
+                rectangle.getVertices().stream()
+                    .allMatch(coordinate -> coordinate.isContainedInPolygon(lineSegments)))
+        .collect(Collectors.toList())
+        .getFirst()
+        .getArea();
   }
 
-  @Builder
   @Getter
   public static class Coordinate {
     private final Long x;
     private final Long y;
 
+    public Coordinate(Long x, Long y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    public boolean isContainedInPolygon(List<LineSegment> lineSegments) {
+      if (lineSegments.stream().anyMatch(lineSegment -> isOnLineSegment(lineSegment))) {
+        return true;
+      }
+
+      LineSegment lineSegmentToStart = new LineSegment(new Coordinate(0L, getY()), this);
+      LineSegment lineSegmentToTop = new LineSegment(new Coordinate(getX(), 0L), this);
+
+      Long verticalCollisions =
+          lineSegments.stream()
+              .filter(lineSegmentToStart::collidesWith)
+              .collect(Collectors.counting());
+      Long horizontalCollisions =
+          lineSegments.stream()
+              .filter(lineSegmentToTop::collidesWith)
+              .collect(Collectors.counting());
+
+      return verticalCollisions % 2 != 0 && horizontalCollisions % 2 != 0;
+    }
+
+    public boolean isOnLineSegment(LineSegment lineSegment) {
+      Long x = getX();
+      Long y = getY();
+      Long ax = lineSegment.getStart().getX();
+      Long ay = lineSegment.getStart().getY();
+      Long bx = lineSegment.getEnd().getX();
+      Long by = lineSegment.getEnd().getY();
+
+      return (x <= Math.max(ax, bx)
+          && x >= Math.min(ax, bx)
+          && y <= Math.max(ay, by)
+          && y >= Math.min(ay, by));
+    }
+
+    public static Orientation getTripletOrientation(Coordinate a, Coordinate b, Coordinate c) {
+      Long ax = a.getX();
+      Long ay = a.getY();
+      Long bx = b.getX();
+      Long by = b.getY();
+      Long cx = c.getX();
+      Long cy = c.getY();
+
+      Long orientation = (by - ay) * (cx - bx) - (bx - ax) * (cy - by);
+
+      if (orientation == 0) return Orientation.COLINEAR;
+      if (orientation > 0) return Orientation.CLOCKWISE;
+      return Orientation.COUNTERCLOCKWISE;
+    }
+
     @Override
     public String toString() {
       return new ObjectMapper().writeValueAsString(this);
     }
+  }
+
+  @Getter
+  public static class LineSegment {
+    private final Coordinate start;
+    private final Coordinate end;
+
+    public LineSegment(Coordinate start, Coordinate end) {
+      this.start = start;
+      this.end = end;
+    }
+
+    public Direction getDirection() {
+      if (getStart().getX().equals(getEnd().getX())) return Direction.VERTICAL;
+      if (getStart().getY().equals(getEnd().getY())) return Direction.HORIZONTAL;
+      throw new RuntimeException(
+          String.format(
+              "Could not get Direction for LineSegment with start '%s' and end '%s'",
+              this.getStart(), this.getEnd()));
+    }
+
+    public boolean collidesWith(LineSegment lineSegment) {
+      if (getStart().isOnLineSegment(lineSegment) && getEnd().isOnLineSegment(lineSegment)) {
+        return true;
+      }
+
+      if (lineSegment.getStart().isOnLineSegment(this)
+          && lineSegment.getEnd().isOnLineSegment(this)) {
+        return true;
+      }
+
+      Orientation orientationOne =
+          Coordinate.getTripletOrientation(getStart(), getEnd(), lineSegment.getStart());
+      Orientation orientationTwo =
+          Coordinate.getTripletOrientation(getStart(), getEnd(), lineSegment.getEnd());
+      Orientation orientationThree =
+          Coordinate.getTripletOrientation(
+              lineSegment.getStart(), lineSegment.getEnd(), getStart());
+      Orientation orientationFour =
+          Coordinate.getTripletOrientation(lineSegment.getStart(), lineSegment.getEnd(), getEnd());
+
+      return !orientationOne.equals(orientationTwo) && !orientationThree.equals(orientationFour);
+    }
+
+    @Override
+    public String toString() {
+      return new ObjectMapper().writeValueAsString(this);
+    }
+  }
+
+  @Getter
+  public static class Rectangle {
+    private final Coordinate vertexOne;
+    private final Coordinate vertexTwo;
+    private final Coordinate vertexThree;
+    private final Coordinate vertexFour;
+
+    public Rectangle(Coordinate vertexOne, Coordinate vertexTwo) {
+      this.vertexOne = vertexOne;
+      this.vertexTwo = vertexTwo;
+      this.vertexThree = new Coordinate(vertexOne.getX(), vertexTwo.getY());
+      this.vertexFour = new Coordinate(vertexTwo.getX(), vertexOne.getY());
+    }
+
+    public List<Coordinate> getVertices() {
+      return List.of(getVertexOne(), getVertexTwo(), getVertexThree(), getVertexFour());
+    }
+
+    public Long getArea() {
+      return (Math.abs(getVertexOne().getY() - getVertexTwo().getY()) + 1)
+          * (Math.abs(getVertexOne().getX() - getVertexTwo().getX()) + 1);
+    }
+
+    @Override
+    public String toString() {
+      return new ObjectMapper().writeValueAsString(this);
+    }
+  }
+
+  public static enum Direction {
+    VERTICAL,
+    HORIZONTAL
+  }
+
+  public static enum Orientation {
+    CLOCKWISE,
+    COUNTERCLOCKWISE,
+    COLINEAR
   }
 }
--- app/src/test/java/org/example/day09/Day09Test.java
+++ app/src/test/java/org/example/day09/Day09Test.java
@@ -4,12 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
 import java.util.List;
+import org.example.day09.Day09.Coordinate;
+import org.example.day09.Day09.Direction;
+import org.example.day09.Day09.LineSegment;
 import org.junit.jupiter.api.Test;
 
 public class Day09Test {
   private final Day09 day09;
-  private final List<Day09.Coordinate> exampleRedTiles;
-  private final List<Day09.Coordinate> redTiles;
+  private final List<Coordinate> exampleRedTiles;
+  private final List<Coordinate> redTiles;
 
   public Day09Test() throws IOException {
     this.day09 = new Day09();
@@ -28,12 +31,78 @@ public class Day09Test {
   }
 
   @Test
+  public void testGetDirection() {
+    assertEquals(
+        Direction.VERTICAL,
+        new LineSegment(new Coordinate(0L, 0L), new Coordinate(0L, 10L)).getDirection());
+    assertEquals(
+        Direction.HORIZONTAL,
+        new LineSegment(new Coordinate(0L, 0L), new Coordinate(10L, 0L)).getDirection());
+  }
+
+  @Test
+  public void testCollidesWith() {
+    assertEquals(
+        true,
+        new LineSegment(new Coordinate(5L, 5L), new Coordinate(9L, 5L))
+            .collidesWith(new LineSegment(new Coordinate(7L, 7L), new Coordinate(7L, 3L))));
+    assertEquals(
+        true,
+        new LineSegment(new Coordinate(9L, 5L), new Coordinate(5L, 5L))
+            .collidesWith(new LineSegment(new Coordinate(7L, 7L), new Coordinate(7L, 3L))));
+    assertEquals(
+        true,
+        new LineSegment(new Coordinate(7L, 7L), new Coordinate(7L, 3L))
+            .collidesWith(new LineSegment(new Coordinate(5L, 5L), new Coordinate(9L, 5L))));
+    assertEquals(
+        true,
+        new LineSegment(new Coordinate(7L, 7L), new Coordinate(7L, 3L))
+            .collidesWith(new LineSegment(new Coordinate(9L, 5L), new Coordinate(5L, 5L))));
+  }
+
+  @Test
+  public void testIsOnLineSegment() {
+    LineSegment horizontalLineSegment =
+        new LineSegment(new Coordinate(5L, 5L), new Coordinate(9L, 5L));
+    LineSegment verticalLineSegment =
+        new LineSegment(new Coordinate(7L, 7L), new Coordinate(7L, 3L));
+    assertEquals(true, new Coordinate(7L, 5L).isOnLineSegment(horizontalLineSegment));
+    assertEquals(true, new Coordinate(5L, 5L).isOnLineSegment(horizontalLineSegment));
+    assertEquals(false, new Coordinate(7L, 6L).isOnLineSegment(horizontalLineSegment));
+    assertEquals(true, new Coordinate(7L, 5L).isOnLineSegment(verticalLineSegment));
+    assertEquals(true, new Coordinate(7L, 7L).isOnLineSegment(verticalLineSegment));
+    assertEquals(false, new Coordinate(8L, 5L).isOnLineSegment(verticalLineSegment));
+    assertEquals(
+        false,
+        new Coordinate(2L, 1L)
+            .isOnLineSegment(new LineSegment(new Coordinate(2L, 3L), new Coordinate(2L, 5L))));
+    assertEquals(
+        false,
+        new Coordinate(2L, 1L)
+            .isOnLineSegment(new LineSegment(new Coordinate(7L, 1L), new Coordinate(11L, 1L))));
+  }
+
+  @Test
+  public void testIsContainedInPolygon() {
+    List<LineSegment> lineSegments =
+        List.of(
+            new LineSegment(new Coordinate(5L, 5L), new Coordinate(5L, 9L)),
+            new LineSegment(new Coordinate(5L, 9L), new Coordinate(9L, 9L)),
+            new LineSegment(new Coordinate(9L, 9L), new Coordinate(9L, 5L)),
+            new LineSegment(new Coordinate(9L, 5L), new Coordinate(5L, 5L)));
+    assertEquals(true, new Coordinate(7L, 7L).isContainedInPolygon(lineSegments));
+    assertEquals(true, new Coordinate(9L, 7L).isContainedInPolygon(lineSegments));
+    assertEquals(false, new Coordinate(11L, 7L).isContainedInPolygon(lineSegments));
+    assertEquals(false, new Coordinate(7L, 3L).isContainedInPolygon(lineSegments));
+  }
+
+  @Test
   public void testExample02() {
     assertEquals(24L, day09.solvePart02(exampleRedTiles));
   }
 
   // @Test
   // public void testPart02() {
-  //   assertEquals(1026594680, day09.solvePart02(redTiles));
+  //   assertEquals(0L, day09.solvePart02(redTiles));
   // }
 }
